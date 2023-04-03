4cc89c7b772f3304cf1832bcb909c20a124f1ccf
alannajamal <alanna.jamal@yahoo.com>
2023-04-03 13:00:39 -0700

    tried to add hit detection

--- script.js
+++ script.js
@@ -7,8 +7,16 @@ function jump(){
     setTimeout(function(){
         character.classList.remove("animate");
     },500);
+}
 
 var checkDead = setInterval (function(){
-
-},10);
+    var characterTop = 
+parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+var blockLeft = 
+parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+if(blockLeft<40 && blockLeft>0 && characterTop>=130){
+    block.style.animation = "none";
+    block.style.display = "none";
+    alert("Game over");
 }
+},10);
