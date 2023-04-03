7cc309c226dcf5dbfcbfb21b267bce2113b99665
alannajamal <alanna.jamal@yahoo.com>
2023-04-03 12:46:32 -0700

    added making sure character doesnt jump too much

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 var character = document.getElementById("character"); 
 var block = document.getElementById("block");
 function jump(){
+    if(character.classList !="animate"){
+    }
     character.classList.add("animate");
-    setTimeout(function(){character.classList.remove("animate")},500);
-}
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },500);
+
 var checkDead = setInterval (function(){
 
 },10);
+}
